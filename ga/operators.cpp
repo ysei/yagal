a2aaed8bee1e158451a77f7981ad346caab746a6
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2012-03-23 13:47:49 +0200

    Implemented crossover

--- ga/operators.cpp
+++ ga/operators.cpp
@@ -8,7 +8,27 @@ namespace Operators
 
 void crossover(byte * solutionDad, byte * solutionMom, uint size, uint point)
 {
+    assert(size * 8 > point);
 
+    uint byteIndex = point / 8;
+    uint bitIndex = point % 8;
+
+    for(uint i = byteIndex + 1; i < size; i++) {
+        solutionDad[i] = solutionMom[i];
+    }
+
+    if(bitIndex != 0) {
+        byte crossoverByteDad = solutionDad[byteIndex];
+        byte crossoverByteMom = solutionMom[byteIndex];
+
+        crossoverByteDad >>= bitIndex;
+        crossoverByteDad <<= bitIndex;
+
+        crossoverByteMom <<= (8 - bitIndex);
+        crossoverByteMom >>= (8 - bitIndex);
+
+        solutionDad[byteIndex] = crossoverByteDad | crossoverByteMom;
+    }
 }
 
 void mutation(byte * solution, uint size, uint point)
--- gatest/operators_test.h
+++ gatest/operators_test.h
@@ -39,4 +39,47 @@ TEST(OperatorsTest, testMutationTwoBytes)
     EXPECT_EQ(K, b3[1]);
 }
 
+TEST(OperatorsTest, testOneByteCrossover)
+{
+    byte b1 = 0x13;
+    byte b2 = 0x25;
+    const byte expected = 0x15;
+
+    // Left half of the byte will be taken from dad
+    // Right half of the byte will be taken from mom
+    Operators::crossover(&b1, &b2, 1, 4);
+    EXPECT_EQ(expected, b1);
+}
+
+TEST(OperatorsTest, testOneBytePoint2Crossover)
+{
+    byte b1 = 0xFE;
+    byte b2 = 0x00;
+    const byte expected = 0x80;
+
+    Operators::crossover(&b1, &b2, 1, 7);
+    EXPECT_EQ(expected, b1);
+}
+
+TEST(OperatorsTest, testOneBytePointIs2Crossover)
+{
+    byte b1 = 0xFE;
+    byte b2 = 0x00;
+    const byte expected = 0xFC;
+
+    Operators::crossover(&b1, &b2, 1, 2);
+    EXPECT_EQ(expected, b1);
+}
+
+TEST(OperatorsTest, testThreeBytesCrossover)
+{
+    byte b1[3] = {0x32, 0x2A, 0x19};
+    byte b2[3] = {0xA2, 0x35, 0x89};
+
+    Operators::crossover(b1, b2, 3, 12);
+    EXPECT_EQ(0x32, b1[0]); // Comes from dad
+    EXPECT_EQ(0x25, b1[1]); // Comes from both
+    EXPECT_EQ(0x89, b1[2]); // Comes from mom
+}
+
 #endif // OPERATORS_TEST_H

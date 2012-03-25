a718ae8e3ddb5e21104a8128fd22672a189acde8
Sergiy Dubovik <sergiy.dubovik@gmail.com>
2012-03-25 14:32:44 +0300

    Range is fixed, unit test is updated

--- ga/range.h
+++ ga/range.h
@@ -2,24 +2,27 @@
 #define RANGE_H
 
 template <class T>
-struct Range
+class Range
 {
-    Range(T range_min, T range_max) : m_range_min(range_min), m_range_max(range_max) {
-        float min = std::numeric_limits<T>::min();
-        float max = std::numeric_limits<T>::max();
 
-        // factor = (float) ((float)(m_range_max - m_range_min)) / 2.f / (max - min);
-        factor = (float)(m_range_max - m_range_min) / (max - min);
+public:
+    Range(T range_min, T range_max) : m_range_min(range_min), m_range_max(range_max) {
+        m_min = std::numeric_limits<T>::min() / 2;
+        T max = std::numeric_limits<T>::max() / 2;
+
+        // Taken from http://stackoverflow.com/questions/5294955/how-to-scale-down-a-range-of-numbers-with-a-known-min-and-max-value
+        factor = (float)(m_range_max - m_range_min) / (max - m_min);
     }
 
     T scaleToRange(T value) {
-        return m_range_min + factor * value;
+        return factor * (value / 2 - m_min) + m_range_min;
     }
 
+private:
     T m_range_min;
     T m_range_max;
+    T m_min;
     float factor;
 };
-
 
 #endif // RANGE_H
--- gatest/range_test.h
+++ gatest/range_test.h
@@ -4,12 +4,35 @@
 #include "../ga/range.h"
 #include "gtest/gtest.h"
 
-TEST(RangeTest, CharTest) {
-    Range<char> r(-12, 10);
+template<class T>
+void testScaleToRange(T min, T max, T value, T expected)
+{
+    Range<T> r(min, max);
 
-    // EXPECT_EQ(10, r.scaleToRange(std::numeric_limits<char>::max()));
-    // EXPECT_EQ(1, r.scaleToRange(std::numeric_limits<char>::min()));
-    // EXPECT_EQ(5, r.scaleToRange(0));
+    EXPECT_EQ(max, r.scaleToRange(std::numeric_limits<T>::max()));
+    EXPECT_EQ(min, r.scaleToRange(std::numeric_limits<T>::min()));
+    EXPECT_EQ(expected, r.scaleToRange(value));
+}
+
+TEST(RangeTest, TestPositiveScale) {
+    testScaleToRange<char>(0, 10, 0, 5);
+    testScaleToRange<short>(0, 10, 0, 5);
+    testScaleToRange<int>(0, 10, 0, 5);
+    testScaleToRange<long>(0, 10, 0, 5);
+}
+
+TEST(RangeTest, TestPositiveAndNegativeScale) {
+    testScaleToRange<char>(-10, 10, 0, 0);
+    testScaleToRange<short>(-10, 10, 0, 0);
+    testScaleToRange<int>(-10, 10, 0, 0);
+    testScaleToRange<long>(-10, 10, 0, 0);
+}
+
+TEST(RangeTest, TestNegativeScale) {
+    testScaleToRange<char>(-10, 0, 0, -4);
+    testScaleToRange<short>(-10, 0, 0, -4);
+    testScaleToRange<int>(-10, 0, 0, -5);
+    testScaleToRange<long>(-10, 0, 0, -5);
 }
 
 #endif
